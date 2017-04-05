8e6229524e73991e04da19641ccf9e7a9b58c5f2
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-04-05 16:51:32 +0300

    test revision is complete

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Исходные данные: список вопросов и вариантов ответов, а также правильные ответы
 var testlist = [
   {
     question: "Вопрос № 1"
@@ -19,7 +20,7 @@ testlist[0].answers = ["Вариант ответа № 1", "Вариант от
 testlist[0].correct = [2];
 
 testlist[1].answers = ["Вариант ответа № 1", "Вариант ответа № 2", "Вариант ответа № 3"];
-testlist[1].correct = [2,3];
+testlist[1].correct = [2, 3];
 
 testlist[2].answers = ["Вариант ответа № 1", "Вариант ответа № 2", "Вариант ответа № 3"];
 testlist[2].correct = [1];
@@ -27,25 +28,33 @@ testlist[2].correct = [1];
 testlist[3].answers = ["Вариант ответа № 1", "Вариант ответа № 2"];
 testlist[3].correct = [1];
 
-
+// преобразование и запись в localStorage
 
 console.log(testlist);
 
-var a = JSON.stringify(testlist);
+var pack = JSON.stringify(testlist);
 
-console.log(a);
+console.log(pack);
 
-localStorage["test"] = a;
+localStorage["test"] = pack;
 
-var b = localStorage.getItem("test");
+// получение данных из localStorage и преобразование
 
-var c = JSON.parse(b)
+var read = localStorage.getItem("test");
 
-console.log(c);
+var revive = JSON.parse(read)
+
+console.log(revive);
+
+// Объявление переменных
 
 var qaItem = [];
 var checkbox = [];
 var paragraph = [];
+var check = [];
+var a = [];
+
+// Создание DOM элементов
 
 var wrapper = document.createElement("form");
 wrapper.className = "pure-form-stacked";
@@ -56,30 +65,51 @@ header.innerHTML = "Тест по программированию";
 header.style.margin = "0 0 50px 0";
 header.style.textAlign = "center";
 
-var submitButton = document.createElement("button");
-submitButton.type = "submit";
+var submitButton = document.createElement("input");
+submitButton.type = "button";
 submitButton.className = "pure-button pure-button-primary";
-submitButton.innerHTML = "Проверить мои результаты";
+submitButton.value = "Проверить мои результаты";
 submitButton.style.margin = "50px 0 0 0";
 submitButton.addEventListener("click", revision);
 
+// Функция сбора ответов и проверка на соответствие правильным
 
 function revision() {
+  var allAnswers = document.forms[0];
 
+  for (i = 0; i < allAnswers.length; i++) {
+        if (allAnswers[i].checked) {
+          check.push(allAnswers[i].name);
+        }
+    }
+  console.log(check);
+  revive.forEach( function(correct, i) {
+    revive[i].correct.forEach( function (item, j, correct) {
+      a.push("a" + i + (+item - 1));
+    })
+  })
+  console.log(a);
+  // Проверка ответов
+  if (check.join(",") === a.join(",")) {
+    alert("Молодец!");
+    return;
+  }
+  alert("Тест не пройден");
+  return;
 };
 
-
+// Помещение элемнтов на страницу
 
 document.body.appendChild(wrapper);
 wrapper.appendChild(header);
 
-for (var i = 0; i < c.length; i++) {
-  qaItem[i] = document.createElement("h1");
-  qaItem[i].innerHTML = ((i+1) + ". " + c[i].question + "\n");
+for (var i = 0; i < revive.length; i++) {
+  qaItem[i] = document.createElement("h2");
+  qaItem[i].innerHTML = ((i+1) + ". " + revive[i].question);
 
   wrapper.appendChild(qaItem[i]);
 
-  c[i].answers.forEach(function(item, j, answers) {
+  revive[i].answers.forEach(function(item, j, answers) {
     paragraph[j] = document.createElement("p");
     var text = document.createTextNode(item);
 
@@ -92,7 +122,6 @@ for (var i = 0; i < c.length; i++) {
     wrapper.appendChild(paragraph[j]);
     paragraph[j].appendChild(checkbox[j]);
     paragraph[j].appendChild(text);
-
   });
 }
 
